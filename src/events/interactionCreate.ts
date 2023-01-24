728791a103464c4dfce2b69ad32212865f8e578f
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2023-01-24 14:22:47 +0000

    Update interactionCreate.ts

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,4 +1,6 @@
 import createDatabaseDocument = require('../modules/createDatabaseDocument');
+import profileSchema = require('../schemas/profileSchema');
+import guildSchema = require('../schemas/guildSchema');
 
 module.exports = {
     name: 'interactionCreate',
@@ -10,8 +12,8 @@ module.exports = {
 
                 if (!command) return; // If the command is not found, return
 
-                const profileData: ProfileDocument = await createDatabaseDocument.createProfileDocument(interaction.user.id);
-                const guildData: GuildDocument = await createDatabaseDocument.createGuildDocument(interaction.guild.id);
+                const profileData: ProfileDocument = await profileSchema.findOneAndUpdate({ userID: interaction.user.id }, { userID: interaction.user.id }, { upsert: true, new: true }); // Get the profile data from the database
+                const guildData: GuildDocument = await guildSchema.findOneAndUpdate({ guildID: interaction.guild.id }, { guildID: interaction.guild.id }, { upsert: true, new: true }); // Get the guild data from the database
 
                 try {
                     await command.execute({ client, interaction, profileData, guildData }); // Execute the command
