1d25711ec4ffdfd68f042f7faf3a8693243faf6d
PWalkersCrisps <61799901+PWalkersCrisps@users.noreply.github.com>
2023-03-21 09:24:54 +0000

    Fixed issue where bot can crash with sticky roles
    This happens because the bot tries to give roles that is higher than its privilege to members

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,6 +1,7 @@
 import profileRoleSchema from '../schemas/profileRoleSchema';
 import guildSchema from '../schemas/guildSchema';
 import { GuildMember, Events } from 'discord.js';
+import { client } from '..';
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(member: GuildMember) {
@@ -13,10 +14,13 @@ module.exports = {
         if (!guildData.stickyRoles) return;
 
         const roles: string[] = profileData.roleIDs;
+        const clientMember: GuildMember | undefined = member.guild.members.cache.get(client.user?.id as string);
+        if (!clientMember) return;
 
         for (const role of roles) {
             const guildRole = member.guild.roles.cache.find((r: any) => r.id === role);
             if (!guildRole) continue;
+            if (guildRole.position >= clientMember.roles?.highest?.position) continue;
             if (member.guild.roles.everyone.id === guildRole.id) continue;
             try {
                 member.roles.add(guildRole);
