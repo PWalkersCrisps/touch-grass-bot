3cc633597df662d3e65d9e8e3793cee9d2682daf
Ashley Walker <61799901+PWalkersCrisps@users.noreply.github.com>
2023-09-11 17:09:39 +0100

    Slur filter

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,70 @@
+import { EmbedBuilder, Events, Message, TextChannel } from 'discord.js';
+import BotEvent from '../classes/event';
+import { config } from 'dotenv';
+
+import slursExact from '../data/slursExact.json';
+import slursWildcard from '../data/slursWildcard.json';
+
+config();
+
+const modLogsChannelID = '826283823884927038';
+const modRoleID = '811018391913627688';
+
+class MessageCreate extends BotEvent {
+
+    constructor() {
+        super();
+        this.name = Events.MessageCreate;
+    }
+
+    async execute(message: Message) {
+        if (this.containsSlur(message)) {
+            this.removeSlur(message);
+        }
+    }
+
+    containsSlur(message: Message): boolean {
+        const wildcardRegexes: RegExp[] = slursWildcard.map((slur: string) => new RegExp('\\b' + slur + '\\b', 'i'));
+
+        const wildcardMatch: boolean = wildcardRegexes.some((regex: RegExp) => regex.test(message.content));
+
+        const exactMatch: boolean = slursExact.some((slur: string) => message.content.toLowerCase().includes(slur.toLowerCase()));
+
+        return wildcardMatch || exactMatch;
+    }
+
+    async removeSlur(message: Message): Promise<void> {
+        const modLogsChannel: TextChannel | undefined = message.client.channels.cache.get(modLogsChannelID) as TextChannel | undefined;
+
+        let modLogsMessage: Message | undefined;
+
+        if (modLogsChannel) {
+
+            const modLogsEmbed = new EmbedBuilder()
+                .setTitle('Slur Filter')
+                .setDescription(`<@${message.author.id}> sent a slur`)
+                .setFields(
+                    { name: 'Message', value: `<@${message.author.id}>: ${message.content}` },
+                );
+
+            modLogsMessage = await modLogsChannel.send({ embeds: [modLogsEmbed] });
+        }
+
+        const replyMessage = `||<@&${modRoleID}>||`;
+        const replyEmbed = new EmbedBuilder()
+            .setTitle('Slur Filter')
+            .setDescription(`<@${message.author.id}> Said a slur!!`);
+
+        if (modLogsMessage) {
+            replyEmbed.addFields({
+                name: 'Mod Logs Link:',
+                value: modLogsMessage.url,
+            });
+        }
+
+        message.reply({ content:  replyMessage, embeds: [replyEmbed] });
+        message.delete();
+    }
+}
+
+export default new MessageCreate();
